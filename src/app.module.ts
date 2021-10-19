14d65dfb3a99bb2f1e6b3c863954bcf70210ab9f
MaximVlasov68 <c.ronal9919@gmail.com>
2021-10-19 22:38:27 +0300

    add auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,13 +5,17 @@ import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
 import { CategoryModule } from './category/category.module';
 import { ManufacturerModule } from './manufacturer/manufacturer.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     ProductModule, 
     MongooseModule.forRoot('mongodb+srv://max:max123@cluster0.g3rgo.mongodb.net/store?retryWrites=true&w=majority'),
     CategoryModule,
-    ManufacturerModule
+    ManufacturerModule,
+    AuthModule,
+    UsersModule
   ],  
   controllers: [AppController],
   providers: [AppService],
--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,19 @@ import { join } from 'path';
 import { AppModule } from './app.module';
 import * as handlebarsHelpers from 'handlebars-helpers';
 import * as hbs from 'hbs';
+import * as session from 'express-session';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(
     AppModule,
+  );
+
+  app.use(
+    session({
+      secret: 'my-secret',
+      resave: false,
+      saveUninitialized: false,
+    }),
   );
 
   app.useStaticAssets(join(__dirname, '..', 'public'));
@@ -15,7 +24,7 @@ async function bootstrap() {
   app.setViewEngine('hbs');
   
   hbs.handlebars.registerHelper({...handlebarsHelpers()})
-    
+
   await app.listen(3000);
 }
 bootstrap();
