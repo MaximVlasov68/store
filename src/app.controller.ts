9bac53e4d8c62f10a2c93e07e9aae20244ab0067
MaximVlasov68 <c.ronal9919@gmail.com>
2021-10-19 22:35:49 +0300

    add login in app controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Post, Render, Request, Res, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { Response } from 'express';
+
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  
+  @UseGuards(AuthGuard('local'))
+  @Post('auth/login')
+  async login(@Request() req, @Res() res: Response) {
+    if(req.user){
+      req.session.user = req.user;
+    }
+    return res.redirect('/product');
   }
+
+  @Render('loginForm')
+  @Get('login')
+  async loginForm(@Res() res: Response){
+    
+  }
+  
 }
