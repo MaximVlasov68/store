d82ce2ebacc49116cf834ff02076c24b65e19dd6
MaximVlasov68 <c.ronal9919@gmail.com>
2021-12-16 06:22:20 +0300

    work on the slider

--- public/js/slider.js
+++ public/js/slider.js
@@ -1,16 +1,21 @@
 var swiper = new Swiper(".mySwiper", {
     cssMode: true,
     autoplay: {
-        delay: 7000,
+        delay: 4000,
     },
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
+      
     },
     pagination: {
       el: ".swiper-pagination",
+      clickable: true,
     },
     loop: true,
     mousewheel: true,
     keyboard: true,
+    
   });
+
+ 
