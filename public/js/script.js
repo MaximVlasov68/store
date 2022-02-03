15f6f245cebef58962b7e14b2e243a5d0728de5e
MaximVlasov68 <c.ronal9919@gmail.com>
2022-02-04 02:01:43 +0300

    work on the form for the user

--- public/js/script.js
+++ public/js/script.js
@@ -3,32 +3,38 @@ let modal = document.querySelector('.modal');
 let login  = document.querySelector('.login');
 let loginBtn = document.querySelector('.login-btn');
 let regBtn = document.querySelector('.reg-btn');
-
+let overlay = document.querySelector('.overlay');
+let inputReg = document.querySelectorAll('.reg');
+console.log(inputReg);
 
 function openModal(){
     modal.style.display = "block";
+    overlay.style.display = "block";
     
 }
 function closeModal(){
     modal.style.display = "none";
+    overlay.style.display = "none";
     
 }
 function select(){
     if(loginBtn.classList.contains('active')){
         /* console.log("регистрация"); */
-        loginBtn.classList.remove('active')
-        regBtn.classList.add('active')
+        loginBtn.classList.remove('active');
+        regBtn.classList.add('active');
+        inputReg.forEach((element) => {
+            console.log(element)
+            element.style.display = "block";
+          })
     } else if(regBtn.classList.contains('active')){
-        regBtn.classList.remove('active')
-        loginBtn.classList.add('active')
+        regBtn.classList.remove('active');
+        loginBtn.classList.add('active');
+        inputReg.forEach((element) => {
+            console.log(element)
+            element.style.display = "none";
+          })
        /*  console.log("авторизация");
         loginBtn.classList.add('active') */
         
-    }
-}
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
     }
 }
