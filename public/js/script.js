304547967bb8e91dfb9afd33b405f5c0a160c21a
MaximVlasov68 <c.ronal9919@gmail.com>
2022-02-08 01:53:32 +0300

    working on the product page and finalizing the form

--- public/js/script.js
+++ public/js/script.js
@@ -5,6 +5,9 @@ let loginBtn = document.querySelector('.login-btn');
 let regBtn = document.querySelector('.reg-btn');
 let overlay = document.querySelector('.overlay');
 let inputReg = document.querySelectorAll('.reg');
+let saveMe = document.querySelector('.saveMe');
+let singIn = document.querySelector('.singIn');
+
 console.log(inputReg);
 
 function openModal(){
@@ -22,6 +25,8 @@ function select(){
         /* console.log("регистрация"); */
         loginBtn.classList.remove('active');
         regBtn.classList.add('active');
+        saveMe.style.display = "none";
+        singIn.innerHTML = "Зарегистрироваться";
         inputReg.forEach((element) => {
             console.log(element)
             element.style.display = "block";
@@ -29,6 +34,8 @@ function select(){
     } else if(regBtn.classList.contains('active')){
         regBtn.classList.remove('active');
         loginBtn.classList.add('active');
+        saveMe.style.display = "block";
+        singIn.innerHTML = "Войти в аккаунт";
         inputReg.forEach((element) => {
             console.log(element)
             element.style.display = "none";
