0c880d8b602af48c3d25c8759c28f33c991fe14f
MaximVlasov68 <c.ronal9919@gmail.com>
2023-10-12 20:08:50 +0300

    add params connect postgress

--- src/main.ts
+++ src/main.ts
@@ -43,7 +43,9 @@ async function bootstrap() {
           host: process.env.POSTGRES_HOST,
           port: process.env.POSTGRES_PORT,
           user: process.env.POSTGRES_USER,
+          database: process.env.POSTGRES_DB,
           password: process.env.POSTGRES_PASSWORD,
+
         },
         createTableIfMissing: true /* создать таблицу автоматом */,
       }),
